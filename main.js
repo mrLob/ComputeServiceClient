c5c7901a57f806e215bbdfd73c6c22c6e7197982
Unknown <g12fpay@gmail.com>
2017-07-20 13:19:31 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const electron = require("electron")
+const {BrowserWindow, app} = electron
+
+function openWindow(hg = 1000,wd = 750,bord = true,modl = false,load = 'template'){
+    let window = new BrowserWindow(
+        {
+            width: wd,
+            height: hg,
+            frame: bord,
+            modal: modl
+        }
+    );
+    window.loadURL('file:///'+__dirname+'/'+load+'.html');
+}
+
+app.on('ready',openWindow);
